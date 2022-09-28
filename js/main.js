b0c3320cee30ccc2fca680f7800400133d6e0ad0
Alex <oleckciizal@gmail.com>
2022-09-28 19:14:30 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,121 @@
+                //   Переменные //
+
+//  "use strict"; // -- с помощью этой директивы используется только новый стандарт
+
+// a = 15;
+
+// console.log(a);
+
+
+// let number = 5;
+// const leftBorderWidht = 1; // Нельзя перезаписать
+
+// number = 10;
+// console.log(number);
+
+// // leftBorderWidht = 10 ;  --- ошибка
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10 ;
+// console.log(obj);
+
+// var name = 'Ivan';
+
+// {
+//     let result = 50 ;
+
+// }
+
+// console.log(result) ;
+
+                //  Типы данных //
+
+//   let number = 4.6 ;
+//   console.log(-4/0);
+
+//   const person = 'Alex' ;
+
+//   const bool = true;
+
+//   const obj = {
+//     name : "John",
+//     age: 25 ,
+//     isMarried: false
+//   };
+
+//   console.log(obj.name);
+
+//   let arr = ['plum.png', 'orange.jpg', 'apple.bmp', 6,  { }, []];
+//   console.log(arr[1]);
+
+                    // ОБщение с пользователем //
+
+// alert("Hello!");
+
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const answer = prompt("Вас есть 18?", "Yes");
+// console.log(typeof(answer));
+
+// const answers = [];
+// // answers[0] = prompt('Как ваше имя?', '');
+// // answers[1] = prompt('Какая у вас фамилия?', '');
+// // answers[2] = prompt('Сколько вам лет?', '');
+
+// // document.write(answers);
+
+// console.log(typeof(answers));
+
+
+
+                //// Интерполяция /////
+
+            //  Объявление переменных внутри строк или команд
+                
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет. ${user}`);
+
+                //// Операторы в JavaScript //// 
+
+// console.log('arr' + " - objeect");
+// console.log(4 + +"5");
+
+// let incr = 10, 
+//     decr = 10;
+
+// incr++;
+// decr--;
+ 
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+
+// console.log(2*4 == 8);   //  Не строгое равенство сравнивает значения
+// console.log(2*4 === '8');   // а строгое типы данных
+
+// &&          //and
+// ||          //or 
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked && isClose);   //false
+// console.log(isChecked || isClose);  //true
+// console.log(isChecked || !isClose);  //Оператор отрицания - обращает значение в обратное 
+
+
+
+                     // Git //
+
+
+
