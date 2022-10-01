7cf045651a943916fda36da9f76c573dca5dc7fe
Alex Zaloznyi <106338161+alex-zal-19@users.noreply.github.com>
2022-10-01 12:36:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -118,4 +118,50 @@
                      // Git //
 
 
+                     // Условия // 
+
+
+
+// if (4 == 9){
+//     console.log('Ok!');
+// } else{
+//     console.log('Error');
+// }
+
+// const num = '50' ;
+// if (num < 49){
+//     console.log('Error');
+// } else if (num > 100){
+//     console.log('So much');
+// } else {
+//     console.log('Ok!');
+// }
+
+// (num === 50) ?  console.log('Ok!') : console.log('Error');
+
+
+// switch (num) {
+//     case '49': 
+//         console.log('Error');
+//         break;
+//     case '100': 
+//          console.log('Error');
+//          break;
+//     case '50': 
+//          console.log('Good');
+//          break;
+//     default:
+//         console.log('Not today');
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
 
